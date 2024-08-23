8f733496c8d190626e97b4c00fa528b436b1a936
Talha-Ali150 <syed.talha.ali.150@gmail.com>
2024-08-24 04:28:37 +0500

    get categories

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import Card from "./Card";
 
-export default function CardList() {
+const getData = async () => {
+  const res = await fetch(`http://localhost:3000/api/posts/`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("failed");
+  }
+  return res.json();
+};
+
+const CardList = async () => {
+  const data = await getData();
   return (
     <div>
       <h3 className="text-center text-3xl mt-10">Recent Posts</h3>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      {data.map((item) => {
+        return <Card key={item._id} />;
+      })}
     </div>
   );
-}
+};
+export default CardList;
--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,19 +1,45 @@
 import React from "react";
-import CategoryBadge from "./CategoryBadge";
-import { data } from "@/constants/data";
+import "./CategoryList.css";
+import Link from "next/link";
+import Image from "next/image";
 
-export default function CategoryList() {
+const getData = async () => {
+  const res = await fetch(`http://localhost:3000/api/categories/`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("failed");
+  }
+  return res.json();
+};
+
+const CategoryList = async () => {
+  const data = await getData();
   return (
     <div>
       <h3 className="text-3xl text-center my-10">Popular Categories</h3>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 px-4">
-        {data.map((item, index) => (
-          <CategoryBadge
-            key={index}
-            title={item.title}
-            imgSrc={item.imgSrc}
-            color={item.color}
-          />
+        {data?.map((item) => (
+          <Link href={`/blog?cat=${item.slug}`} key={item._id}>
+            <div
+              className={`flex flex-col sm:flex-row justify-center items-center  p-4 rounded-lg shadow-md my-4 w-full sm:w-40 ${item.slug}`}
+            >
+              <div className="w-16 h-16 sm:w-12 sm:h-12 rounded-full overflow-hidden">
+                {item.img && (
+                  <Image
+                    src={item.img}
+                    width={64}
+                    height={64}
+                    alt={item.title}
+                    className="object-cover"
+                  />
+                )}
+              </div>
+              <p className="mt-4 sm:mt-0 sm:ml-3 text-white font-medium text-center sm:text-left">
+                {item.title}
+              </p>
+            </div>
+          </Link>
         ))}
 
         {/* <CategoryBadge
@@ -29,4 +55,6 @@ export default function CategoryList() {
       </div>
     </div>
   );
-}
+};
+
+export default CategoryList;
